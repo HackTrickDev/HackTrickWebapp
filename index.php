22fea2b542049250fa2d5a4004624bed8153db80
Pplaza7 <40174143+Pplaza7@users.noreply.github.com>
2018-06-11 16:52:55 +0200

    Inicial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+
+	<head>
+	
+		<link rel="stylesheet" href="estilologin.css">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+	</head>
+	<body>
+	
+		<div id="contenedor">
+		
+			<nav class="barra"> 
+				<ul class="menu">
+					<li> Crea un equipo y compite contra rivales de todo el mundo! </li>
+					<span id="titulo"> <a href="#"> HACK-TRICK </a> </span>
+				</ul>
+			</nav>
+			
+			<article class="principal">
+			
+			<div id="uno">
+			
+				<h1> REGISTRARSE </h1>
+				<br> <br>
+				<input type="mail"> <b> Correo electrónico  </input>
+				<br> <br>
+				<input type="text"> Nombre de usuario </input>
+				<br> <br>
+				<input type="password">  Contraseña </input>
+				<br> <br>
+				<input type="password">  Repita la contraseña </input>
+				<br> <br>
+				<button> Entrar </button>
+			
+			</div>
+			
+			
+			<div id="dos">
+			
+				<h1> INICIAR SESIÓN </h1>
+				<br> <br> <br> <br>
+				<input type="text"> Nombre de usuario </input>
+				<br> <br>
+				<input type="password">  Contraseña </input>
+				<br> <br> </b>
+				<button> Entrar </button>
+			
+			</div>
+			
+			
+			</article>
+			
+	</body>
+	
+</html>
